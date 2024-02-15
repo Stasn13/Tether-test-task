0324b127a78daffce499951fe7c578fb96be0f99
Stanislav Gavrylenko <stanislav.gavrylenko@konsult.atg.se>
2024-02-15 00:32:01 +0000

    add button ui-element, remove unused styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types';
 
 import styles from './Button.module.scss';
 
-function Button({ children, className, onClick }) {
+function Button({ children, className, onClick, transparent }) {
   return (
     <button
       data-testid="button"
       type="button"
-      className={`${styles.button} ${className}`}
+      className={`${styles.button} ${className} ${
+        transparent && styles.transparent
+      }`}
       onClick={onClick}
     >
       {children}
@@ -20,12 +22,14 @@ Button.defaultProps = {
   children: '',
   className: '',
   onClick: () => null,
+  transparent: false,
 };
 
 Button.propTypes = {
   children: PropTypes.node,
   className: PropTypes.string,
   onClick: PropTypes.func,
+  transparent: PropTypes.bool,
 };
 
 export default Button;
